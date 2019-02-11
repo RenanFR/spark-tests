2e01cf6eb99a43ae1e65e8123c9708fd8d86f696
renan.rodrigues <renan.rodrigues@DESKTOP-MR3FPJG>
2019-02-11 11:52:19 -0200

    Adding used data set and application package to test

--- src/main/java/spark/tests/PlayersByCountrySpark.java
+++ src/main/java/spark/tests/PlayersByCountrySpark.java
@@ -19,32 +19,37 @@ public class PlayersByCountrySpark {
 			System.err.println("Please inform the input and output paths and the player position/country wanted");
 			System.exit(-1);
 		}
+		final String position = args[2];
+		final String country = args[3];
+		String inputPath = args[0];
+		String outputPath = args[1];
+		System.out.println("Selected position " + position + " and country " + country);
+		System.out.println("Input path at " + inputPath + " and output path at " + outputPath);
 		SparkConf sparkConf = new SparkConf();//Used to provide useful properties to the application
 		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);//To communicate with the file system
-		JavaRDD<String> dataset = sparkContext.textFile(args[0]);//Loading file with the path provided on execution
+		JavaRDD<String> dataset = sparkContext.textFile(inputPath);//Loading file with the path provided on execution
 		JavaRDD<String[]> records = dataset.map(new Function<String, String[]>() {
 			public String[] call(String rawData) throws Exception {
-				return rawData.split(",");//If Java 8 runtime is available we can use lambda expressions instead of anonymous class 
+				return rawData.split(";");//If Java 8 runtime is available we can use lambda expressions instead of anonymous class 
 			}
 		});
-		final String position = args[1];
-		final String country = args[2];
+		System.out.println("Number of records: " + records.count());
 		JavaRDD<String[]> filtered = records.filter(new Function<String[], Boolean>() {
 			public Boolean call(String[] records) throws Exception {
-				return records[21] == position && records[5] == country;
+				if (records.length != 18) return false;
+				return records[12].matches(position) && records[2].matches(country);
 			}
 		});
+		System.out.println("Founded players by criteria " + filtered.count());
 		JavaPairRDD<String, String> keyValue  = filtered.mapToPair(new PairFunction<String[], String, String>() {
 			public Tuple2<String, String> call(String[] filtered) throws Exception {
-				return new Tuple2<String, String>(filtered[5], filtered[2]);
+				return new Tuple2<String, String>(filtered[2], filtered[0]);
 			}
 		});
-		JavaPairRDD<String, String> grouped = keyValue.reduceByKey(new Function2<String, String, String>() {
-			public String call(String key, String value) throws Exception {
-				return key;
-			}
-		});
-		grouped.saveAsTextFile(args[3]);
+		JavaPairRDD<String, Iterable<String>> grouped = keyValue.groupByKey();
+		System.out.println("Result count after grouping " + grouped.count());
+		grouped.saveAsTextFile(outputPath);
+		System.out.println("Result file stored at " + outputPath);
 	}
 	
 }
